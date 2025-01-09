fd2a3b41d539fb8c968ee53e9cabb2f863482bd6
parth <parthamaji2929@gmail.com>
2025-01-09 23:35:55 +0530

    New operations Added

--- sample.cpp
+++ sample.cpp
@@ -4,7 +4,14 @@
  int main(){
     int var = 10;
     std::cout << "Hello Mate" << std::endl;
+
+    std::cout << "Multiplication: ";
     std::cout << var * var << std::endl;
+
+    std::cout << "Addition: ";
+    std::cout << var + var << std::endl;
+
+
 
     return 0;
  }
